ac7056c10c5be36dd98311fb4cbab854b26be531
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-11-14 12:56:31 +0100

    the amount of free space on a drive

---
+++ ConsoleSystemDrive14nov2023/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welke drive wil je bekijken: (Hint: 1)");
+        int driveNummer = Convert.ToInt32(Console.ReadLine()) - 1;
+
+        if (driveNummer >= 0 && driveNummer < DriveInfo.GetDrives().Length)
+        {
+            long cdriveinbytes = DriveInfo.GetDrives()[driveNummer].AvailableFreeSpace;
+            long cdriveInMegaBytes = cdriveinbytes / 1_000_000;
+            Console.WriteLine($"The amount of free space on a drive is {cdriveInMegaBytes} Megabytes.");
+            long totalsize = DriveInfo.GetDrives()[driveNummer].TotalSize;
+            long totalsizeInMegaBytes = totalsize / 1_000_000;
+            Console.WriteLine($"The total size of storage space on a drive is {totalsizeInMegaBytes} Megabytes.");
+        }
+
+        Console.WriteLine("Kris Borremans");
+        Console.Read();
+    }
+}
