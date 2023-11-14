478c588bf17736868ab3c03e2ca70314df4bf1ea
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-11-14 12:32:49 +0100

    Linq Average

---
+++ ConsolePrijzenForeachAverage14nov2023/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsolePrijzenForeachAverage14nov2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Prijzen met Foreach en Average
+            {
+                Console.WriteLine("Welkom bij Prijzen met Foreach en Average!");
+
+                const int LENGTH = 3; // 20;
+                double[] array = new double[LENGTH];
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine("Geef een prijs: ");
+                    string input = Console.ReadLine();
+                    if (double.TryParse(input, out double result))
+                    {
+                        array[i] = result;
+                    }
+                }
+
+                Console.WriteLine("De prijzen hoger of gelijk aan 5 zijn: ");
+                foreach (double prijs in array)
+                {
+                    if (prijs >= 5)
+                    {
+                        Console.Write(prijs + "\t");
+                    }
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Het gemiddelde van alle prijzen is " + array.Average());
+                Console.ReadLine();
+            }
+            #endregion
+        }
+    }
+}
