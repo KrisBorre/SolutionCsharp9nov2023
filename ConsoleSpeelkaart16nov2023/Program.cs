7f1a54d9efd3cc6b44f9648084095de65fbf6e42
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-11-16 14:55:02 +0100

    conditional setter

---
+++ ConsoleSpeelkaart16nov2023/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleSpeelkaart16nov2023
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij Speelkaarten!");
+
+            List<Speelkaart16nov2023> kaartenboek = new List<Speelkaart16nov2023>();
+
+            for (int i = 1; i <= 13; i++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    kaartenboek.Add(new Speelkaart16nov2023() { Getal = i, Kleur = (SpeelkaartKleur)k });
+                }
+            }
+
+            Console.WriteLine("Het aantal kaarten is " + kaartenboek.Count); // 52
+
+            Random r = new Random();
+
+            for (int i = 0; i < 52; i++)
+            {
+                int trek = r.Next(0, 52 - i);
+                Speelkaart16nov2023 getrokken = kaartenboek[trek];
+                Console.WriteLine($"{getrokken.Kleur} {getrokken.Getal}");
+                kaartenboek.RemoveAt(trek);
+            }
+
+            Console.WriteLine("Het aantal kaarten is " + kaartenboek.Count); // 0
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleSpeelkaart16nov2023/Speelkaart16nov2023.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleSpeelkaart16nov2023
+{
+    internal enum SpeelkaartKleur { Schoppen, Harten, Klaveren, Ruiten }
+    
+    internal class Speelkaart16nov2023
+    {
+        private int getal;
+        public int Getal
+        {
+            get
+            {
+                return getal;
+            }
+            set
+            {
+                if (value > 0 && value < 14) getal = value;
+                else Console.WriteLine("Fout: Getal kan niet kleiner of gelijk zijn aan nul. Getal kan ook niet groter of gelijk zijn aan 14.");
+            }
+        }
+
+        public SpeelkaartKleur Kleur { get; set; }
+
+    }
+}
