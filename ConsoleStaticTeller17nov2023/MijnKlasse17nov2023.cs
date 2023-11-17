377554ce9b43a75c91cf79dbec1fa1562c169b67
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-11-17 16:14:09 +0100

    static

---
+++ ConsoleStaticTeller17nov2023/MijnKlasse17nov2023.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleStaticTeller17nov2023
+{
+    internal class MijnKlasse17nov2023
+    { 
+        private static int instantieTeller = 0;
+        public const int getal1 = 1;
+        public static readonly int getal2;
+
+        static MijnKlasse17nov2023()
+        {
+            Console.WriteLine("Dit wordt uitgevoerd voor er een instantie van MijnKlasse is gemaakt.");
+            getal2 = 2;
+        }
+
+        // constructor
+        public MijnKlasse17nov2023()
+        {
+            Console.WriteLine("Constructie van een " + this.GetType());
+            instantieTeller++;
+        }
+
+        public static int InstantieTeller
+        {
+            get
+            {
+                return instantieTeller;
+            }
+        }
+
+
+
+    }
+}
---
+++ ConsoleStaticTeller17nov2023/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleStaticTeller17nov2023
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello InstantieTeller!");
+
+            MijnKlasse17nov2023 mijnKlasse1;
+            MijnKlasse17nov2023 mijnKlasse2;
+            Console.WriteLine("InstantieTeller = " + MijnKlasse17nov2023.InstantieTeller);
+            MijnKlasse17nov2023 mijnKlasse3 = new MijnKlasse17nov2023();
+            MijnKlasse17nov2023 mijnKlasse4 = new MijnKlasse17nov2023();
+            Console.WriteLine("InstantieTeller = " + MijnKlasse17nov2023.InstantieTeller);
+            Console.WriteLine("MijnKlasse17nov2023.getal1 = " + MijnKlasse17nov2023.getal1);
+            MijnKlasse17nov2023 mijnKlasse5 = new MijnKlasse17nov2023();
+            Console.WriteLine("MijnKlasse17nov2023.getal2 = " + MijnKlasse17nov2023.getal2);
+            Console.WriteLine("mijnKlasse3.ToString() = " + mijnKlasse3.ToString());
+            Console.WriteLine("InstantieTeller = " + MijnKlasse17nov2023.InstantieTeller);
+
+            // Hello InstantieTeller!
+            // Dit wordt uitgevoerd voor er een instantie van MijnKlasse is gemaakt.
+            // InstantieTeller = 0
+            // Constructie van een ConsoleStaticTeller17nov2023.MijnKlasse17nov2023
+            // Constructie van een ConsoleStaticTeller17nov2023.MijnKlasse17nov2023
+            // InstantieTeller = 2
+            // MijnKlasse17nov2023.getal1 = 1
+            // Constructie van een ConsoleStaticTeller17nov2023.MijnKlasse17nov2023
+            // MijnKlasse17nov2023.getal2 = 2
+            // mijnKlasse3.ToString() = ConsoleStaticTeller17nov2023.MijnKlasse17nov2023
+            // InstantieTeller = 3
+
+            Console.ReadLine();
+        }
+    }
+}
