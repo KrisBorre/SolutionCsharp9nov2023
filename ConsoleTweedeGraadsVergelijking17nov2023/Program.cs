d46852b90d0fcbef0d7771950ea08ec16a86ba58
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-11-17 15:32:48 +0100

    Passing an argument by reference

---
+++ ConsoleTweedeGraadsVergelijking17nov2023/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleTweedeGraadsVergelijking17nov2023
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo TweedeGraadsVergelijking!");
+
+            double x1 = 0; // initialisatie is noodzakelijk
+            double x2 = 0;
+            bool zijnGevonden = false;
+
+            #region oefening 1
+            {
+                TweedeGraadsVergelijking vgl1 = new TweedeGraadsVergelijking(1, 2, 1);
+                Console.WriteLine(vgl1);
+                vgl1.ZoekNulpunten(ref x1, ref x2, ref zijnGevonden);
+                if (zijnGevonden)
+                {
+                    Console.WriteLine("x1 = " + x1); // -1
+                    Console.WriteLine("x2 = " + x2); // -1
+                    Console.WriteLine("vgl1.YWaarde(" + x1 + ") = " + vgl1.YWaarde(x1));
+                    Console.WriteLine("vgl1.YWaarde(" + x2 + ") = " + vgl1.YWaarde(x2));
+                }
+            }
+            #endregion
+
+            #region oefening 2
+            {
+                TweedeGraadsVergelijking vgl2 = new TweedeGraadsVergelijking(2, 3, 1);
+                Console.WriteLine(vgl2);
+                vgl2.ZoekNulpunten(ref x1, ref x2, ref zijnGevonden);
+                if (zijnGevonden)
+                {
+                    Console.WriteLine("x1 = " + x1); // -0,5
+                    Console.WriteLine("x2 = " + x2); // -1
+                    Console.WriteLine("vgl2.YWaarde(" + x1 + ") = " + vgl2.YWaarde(x1));
+                    Console.WriteLine("vgl2.YWaarde(" + x2 + ") = " + vgl2.YWaarde(x2));
+                }
+            }
+            #endregion
+
+            Console.WriteLine("Kris Borremans");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleTweedeGraadsVergelijking17nov2023/TweedeGraadsVergelijking.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleTweedeGraadsVergelijking17nov2023
+{
+    internal class TweedeGraadsVergelijking
+    {
+        private double a, b, c;
+
+        public TweedeGraadsVergelijking(double a, double b, double c)
+        { 
+            this.a = a; this.b = b; this.c = c; 
+        }
+
+        public double YWaarde(double x)
+        {
+            return a * Math.Pow(x, 2) + b * x + c;
+        }
+
+        public double Discriminant
+        { 
+            get { return Math.Pow(b, 2) - 4 * a * c; } 
+        }
+
+        public void ZoekNulpunten(ref double x1, ref double x2, ref bool heeftNulpunten)
+        {
+            double d = this.Discriminant;
+            if (d >= 0)
+            {
+                heeftNulpunten = true;
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+            }
+            else heeftNulpunten = false;
+        }
+
+        public override string ToString()
+        {
+            return a + " * x * x + " + b + " * x + " + c;
+        }
+    }
+}
