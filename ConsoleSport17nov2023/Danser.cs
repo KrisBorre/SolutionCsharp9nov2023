89e6f168f14cca8cd3ccb5829c254af96e77c6ed
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-11-17 16:27:21 +0100

    getters and setters

---
+++ ConsoleSport17nov2023/Danser.cs
@@ -0,0 +1,78 @@
+﻿namespace ConsoleSport17nov2023
+{
+    internal enum Schoenen { Latin, Ballet, Flamenco, Tap, HipHop };
+
+    internal class Danser
+    {
+        public string Naam { get; set; }
+
+        private int leeftijd;
+
+        public int Leeftijd
+        {
+            get { return leeftijd; }
+            set { if (value > 0) { leeftijd = value; } }
+        }
+
+        private Schoenen dansschoenen;
+
+        public Schoenen Dansschoenen
+        {
+            get { return dansschoenen; }
+            set { dansschoenen = value; }
+        }
+
+        private Danser danspartner;
+
+        public Danser Danspartner
+        {
+            get { return danspartner; }
+            set { danspartner = value; }
+        }
+
+        private int aantalWedstrijden;
+
+        public int AantalWedstrijden
+        {
+            get
+            {
+                return aantalWedstrijden;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    aantalWedstrijden = value;
+                }
+            }
+        }
+
+        public void Dans()
+        {
+            Console.WriteLine($"{Naam} danst.");
+        }
+
+        public void Win()
+        {
+            Console.WriteLine($"{Naam} heeft weer een danswedstrijd gewonnen ....");
+        }
+
+        public void StelIn(string naam, int leeftijd, Schoenen dansschoenen, Danser danspartner, int aantalWedstrijden)
+        {
+            this.Naam = naam;
+            this.Leeftijd = leeftijd;
+            this.Dansschoenen = dansschoenen;
+            this.Danspartner = danspartner;
+            this.AantalWedstrijden = aantalWedstrijden;
+        }
+
+        public void Toon()
+        {
+            if (this.Danspartner != null) Console.WriteLine($"Danser {this.Naam} danst met {this.Danspartner.Naam}.");
+            Console.WriteLine($"Danser {this.Naam} heeft {this.AantalWedstrijden} keer gedanst.");
+            Console.WriteLine($"Danser {this.Naam} heeft {this.Dansschoenen} dansschoenen.");
+        }
+    }
+
+
+}
---
+++ ConsoleSport17nov2023/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleSport17nov2023
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij danssport!");
+            Danser tom = new Danser();
+            Danser eva = new Danser();
+            Danser karol = new Danser();
+            Danser brian = new Danser();
+            Danser heidi = new Danser();
+
+            tom.AantalWedstrijden = 10;
+            eva.AantalWedstrijden = 4;
+            brian.AantalWedstrijden = 3;
+            brian.StelIn("Brian", 5, Schoenen.Tap, heidi, brian.AantalWedstrijden);
+            heidi.AantalWedstrijden = 3;
+            heidi.Naam = "Heidi";
+            tom.StelIn("Tom", 12, Schoenen.Latin, eva, tom.AantalWedstrijden);
+            eva.Naam = "Eva";
+            eva.Dansschoenen = Schoenen.Latin;
+            karol.StelIn("Karol", 8, Schoenen.Flamenco, new Danser(), 2);
+            tom.Toon();
+            eva.Toon();
+            karol.Toon();
+            brian.Toon();
+            tom.Dans();
+            tom.Win();
+            Console.ReadLine();
+        }
+    }
+}
